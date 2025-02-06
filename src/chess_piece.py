9b367511c2aefe670ebce609bede5ec97818efad
Dylan <rattledew17@gmail.com>
2025-02-05 23:57:31 -0800

    reworked chess piece data

---
+++ src/chess_piece.py
@@ -0,0 +1,127 @@
+chess_piece_general_data_dict = {
+    "black_king": {
+        "unicode": "♚"},
+    "black_queen": {
+        "unicode": "♛"},
+    "black_rook": {
+        "unicode": "♜"},
+    "black_bishop": {
+        "unicode": "♝"},
+    "black_knight": {
+        "unicode": "♞"},
+    "black_pawn": {
+        "unicode": "♟"},
+    
+    "white_king": {
+        "unicode": "♔"},
+    "white_queen": {
+        "unicode": "♕"},
+    "white_rook": {
+        "unicode": "♖"},
+    "white_bishop": {
+        "unicode": "♗"},
+    "white_knight": {
+        "unicode": "♘"},
+    "white_pawn": {
+        "unicode": "♙"},
+}
+
+chess_piece_specific_data_dict = {
+    "br1": {
+        "name": "black_rook",
+    },
+    "bn1": {
+        "name": "black_knight",
+    },
+    "bb1": {
+        "name": "black_bishop",
+    },
+    "bq": {
+        "name": "black_queen",
+    },
+    "bk": {
+        "name": "black_king",
+    },
+    "bb2": {
+        "name": "black_bishop",
+    },
+    "bn2": {
+        "name": "black_knight",
+    },
+    "br2": {
+        "name": "black_rook",
+    },
+    "bp1": {
+        "name": "black_pawn",
+    },
+    "bp2": {
+        "name": "black_pawn",
+    },
+    "bp3": {
+        "name": "black_pawn",
+    },
+    "bp4": {
+        "name": "black_pawn",
+    },
+    "bp5": {
+        "name": "black_pawn",
+    },
+    "bp6": {
+        "name": "black_pawn",
+    },
+    "bp7": {
+        "name": "black_pawn",
+    },
+    "bp8": {
+        "name": "black_pawn",
+    },
+    
+    "wr1": {
+        "name": "white_rook",
+    },
+    "wn1": {
+        "name": "white_knight",
+    },
+    "wb1": {
+        "name": "white_bishop",
+    },
+    "wq": {
+        "name": "white_queen",
+    },
+    "wk": {
+        "name": "white_king",
+    },
+    "wb2": {
+        "name": "white_bishop",
+    },
+    "wn2": {
+        "name": "white_knight",
+    },
+    "wr2": {
+        "name": "white_rook",
+    },
+    "wp1": {
+        "name": "white_pawn",
+    },
+    "wp2": {
+        "name": "white_pawn",
+    },
+    "wp3": {
+        "name": "white_pawn",
+    },
+    "wp4": {
+        "name": "white_pawn",
+    },
+    "wp5": {
+        "name": "white_pawn",
+    },
+    "wp6": {
+        "name": "white_pawn",
+    },
+    "wp7": {
+        "name": "white_pawn",
+    },
+    "wp8": {
+        "name": "white_pawn",
+    },
+}
--- src/main.py
+++ src/main.py
@@ -1,26 +1,15 @@
+from chess_piece import chess_piece_specific_data_dict, chess_piece_general_data_dict
+
+cpsdd = chess_piece_specific_data_dict # name shortcut
 chess_board_dict = {
-    "a8": "black_rook", "b8": "black_knight", "c8": "black_bishop", "d8": "black_queen", "e8": "black_king", "f8": "black_bishop", "g8": "black_knight", "h8": "black_rook", 
-    "a7": "black_pawn", "b7": "black_pawn", "c7": "black_pawn", "d7": "black_pawn", "e7": "black_pawn", "f7": "black_pawn", "g7": "black_pawn", "h7": "black_pawn", 
+    "a8": cpsdd["br1"]["name"], "b8": cpsdd["bn1"]["name"], "c8": cpsdd["bb1"]["name"], "d8": cpsdd["bq"]["name"], "e8": cpsdd["bk"]["name"], "f8": cpsdd["bb2"]["name"], "g8": cpsdd["bn2"]["name"], "h8": cpsdd["br2"]["name"], 
+    "a7": cpsdd["bp1"]["name"], "b7": cpsdd["bp2"]["name"], "c7": cpsdd["bp3"]["name"], "d7": cpsdd["bp4"]["name"], "e7": cpsdd["bp5"]["name"], "f7": cpsdd["bp6"]["name"], "g7": cpsdd["bp7"]["name"], "h7": cpsdd["bp8"]["name"], 
     "a6": None, "b6": None, "c6": None, "d6": None, "e6": None, "f6": None, "g6": None, "h6": None, 
     "a5": None, "b5": None, "c5": None, "d5": None, "e5": None, "f5": None, "g5": None, "h5": None, 
     "a4": None, "b4": None, "c4": None, "d4": None, "e4": None, "f4": None, "g4": None, "h4": None, 
     "a3": None, "b3": None, "c3": None, "d3": None, "e3": None, "f3": None, "g3": None, "h3": None, 
-    "a2": "white_pawn", "b2": "white_pawn", "c2": "white_pawn", "d2": "white_pawn", "e2": "white_pawn", "f2": "white_pawn", "g2": "white_pawn", "h2": "white_pawn", 
-    "a1": "white_rook", "b1": "white_knight", "c1": "white_bishop", "d1": "white_queen", "e1": "white_king", "f1": "white_bishop", "g1": "white_knight", "h1": "white_rook",
-}
-chess_piece_dict = {
-    "black_king": "♚",
-    "black_queen": "♛",
-    "black_rook": "♜",
-    "black_bishop": "♝",
-    "black_knight": "♞",
-    "black_pawn": "♟",
-    "white_king": "♔",
-    "white_queen": "♕",
-    "white_rook": "♖",
-    "white_bishop": "♗",
-    "white_knight": "♘",
-    "white_pawn": "♙",
+    "a2": cpsdd["wp1"]["name"], "b2": cpsdd["wp2"]["name"], "c2": cpsdd["wp3"]["name"], "d2": cpsdd["wp4"]["name"], "e2": cpsdd["wp5"]["name"], "f2": cpsdd["wp6"]["name"], "g2": cpsdd["wp7"]["name"], "h2": cpsdd["wp8"]["name"], 
+    "a1": cpsdd["wr1"]["name"], "b1": cpsdd["wn1"]["name"], "c1": cpsdd["wb1"]["name"], "d1": cpsdd["wq"]["name"], "e1": cpsdd["wk"]["name"], "f1": cpsdd["wb2"]["name"], "g1": cpsdd["wn2"]["name"], "h1": cpsdd["wr2"]["name"],
 }
 
 def display_chess_board() -> None:
@@ -36,7 +25,7 @@ def display_chess_board() -> None:
             # if the current space has a piece
             else:
                 # adds the piece to the board
-                chess_row += f" {chess_piece_dict[chess_board_dict[letter + str(number)]]}  |"
+                chess_row += f" {chess_piece_general_data_dict[chess_board_dict[letter + str(number)]]["unicode"]}  |"
         print(f"{number} |{chess_row}")
         print("  +----+----+----+----+----+----+----+----+")
         chess_row = ""
@@ -49,16 +38,16 @@ def white_to_move() -> None:
     while is_valid_move is False:
         next_move = input("YOUR TURN: ")
         
-        next_move_notation = next_move[-2:]
+        next_move_full_notation = next_move[-2:]
         next_move_letter_notation = next_move[-2]
         next_move_number_notation = next_move[-1]
         
         # if moving a pawn
         if next_move[0] not in piece_notation:
-            if chess_board_dict[next_move_notation] is None:
+            if chess_board_dict[next_move_full_notation] is None:
                 
                 # moves the pawn up from inital placement
-                chess_board_dict[next_move_notation] = chess_board_dict[next_move_letter_notation + str(int(next_move_number_notation) - 2)]
+                chess_board_dict[next_move_full_notation] = chess_board_dict[next_move_letter_notation + str(int(next_move_number_notation) - 2)]
                 chess_board_dict[next_move_letter_notation + str(int(next_move_number_notation) - 2)] = None
                 is_valid_move = True
 
