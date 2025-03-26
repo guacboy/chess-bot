fbcea67a3f5a401c8a9885e4ee4f9814eded2e11
dyldo <@rattledew17@gmail.com>
2025-03-25 19:30:55 -0700

    added sprite movement

--- src/main.py
+++ src/main.py
@@ -1,62 +1,63 @@
-import pygame
+import pygame as pg
 
-pygame.init()
+pg.init()
 
-screen = pygame.display.set_mode((720, 620))
-clock = pygame.time.Clock()
+screen = pg.display.set_mode((920, 720))
+clock = pg.time.Clock()
 
 def app() -> None:
     is_running = True
     
     # list of objects
     objects = []
-    rectangle = pygame.Rect(100, 100, 100, 200)
-    objects.append(rectangle)
-    square = pygame.Rect(500, 100, 100, 100)
-    objects.append(square)
+    
+    pawn = pg.image.load("../src/assets/w_pawn.png")
+    pawn = pg.transform.scale(pawn, (100, 100))
+    pawn_collision = pawn.get_rect(center=(100, 100))
+    objects.append([pawn, pawn_collision])
+    bishop = pg.image.load("../src/assets/w_bishop.png")
+    bishop = pg.transform.scale(bishop, (100, 100))
+    bishop_collision = bishop.get_rect(center=(200, 100))
+    objects.append([bishop, bishop_collision])
     
     is_moving = False
     
     while is_running:
         screen.fill("black") # color BG
         
-        # loads objects onto screen
-        pygame.draw.rect(screen, (0, 0, 255), rectangle, 0)
-        pygame.draw.rect(screen, (0, 255, 0), square, 0)
+        # loads objects onto screen /
+        # updates object's position
+        for object in objects: 
+            screen.blit(object[0], object[1])
         
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
                 is_running = False
             
             # if left-mouse button down
-            if (event.type == pygame.MOUSEBUTTONDOWN
+            if (event.type == pg.MOUSEBUTTONDOWN
                 and event.button == 1):
-                # iterates through the list of objects
                 for object in objects:
                     # if cursor is inside an object
-                    if object.collidepoint(event.pos):
+                    if object[1].collidepoint(event.pos):
+                        object_collision = object[1]
                         # initialize object to be moved
-                        object_to_be_moved = object
                         is_moving = True
             # if left-mouse button is down
             # and mouse is moving
-            elif (event.type == pygame.MOUSEMOTION
+            elif (event.type == pg.MOUSEMOTION
                 and is_moving):
                 # moves object
-                object_to_be_moved.move_ip(event.rel)
+                object_collision.move_ip(event.rel)
             # if left-mouse button is up
-            elif (event.type == pygame.MOUSEBUTTONUP
+            elif (event.type == pg.MOUSEBUTTONUP
                 and event.button == 1):
                 is_moving = False
-
-        if is_moving:
-            # updates objects position
-            pygame.draw.rect(screen, (0, 0, 255), object_to_be_moved, 0)
         
-        pygame.display.flip() # displays screen
+        pg.display.flip() # displays screen
         clock.tick(60)  # limits FPS to 60
 
-    pygame.quit()
+    pg.quit()
 
 if __name__ == "__main__":
     app()
